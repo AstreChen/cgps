7719e191d5d01a2617295499a8444e243b8a6670
AstreChen <aichen620@gmail.com>
2017-10-20 16:46:02 +0800

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import sys, os
 import subprocess
 from optparse import OptionParser
 
@@ -6,27 +6,27 @@ def config_option():
     usage = 'Usage: main.py -e expfile -p phefile -d datatype -o outdir'
     p = OptionParser(usage)
     p.add_option(
-        '-e','--expfile',dest='expfile',action='store_true',
+        '-e','--expfile',dest='expfile',action='store',
         help='')
     p.add_option(
-        '-p','--phefile',dest='phefile',action='store_true',
+        '-p','--phefile',dest='phefile',action='store',
         help='')
     p.add_option(
-        '-d','--datatype',dest='datatype',action='store_true',
+        '-d','--datatype',dest='datatype',action='store',
         help='')
     p.add_option(
-        '-o','--outdir',dest='outdir',action='store_true',
+        '-o','--outdir',dest='outdir',action='store',
         help='')
     opt, args = p.parse_args()
     return p,opt,args 
 
 opt_parser, opt, args = config_option()
-
+print opt.expfile,opt.phefile,opt.datatype,opt.outdir
 if len(sys.argv) == 1:
     opt_parser.print_help()
     sys.exit(1)
 
-cmdline = 'Rscript test1.R '+
+cmdline = 'Rscript test1.R '+ \
 ' '.join(['-e', opt.expfile, '-p', opt.phefile, '-d', opt.datatype,'-o',opt.outdir])
 
 subprocess.Popen(cmdline)
