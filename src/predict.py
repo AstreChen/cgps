65393622d539cb9d151d4957b534a0e9432cfe42
Ai Chen <aichen@AideiMac.local>
2018-02-26 16:17:31 +0800

    modified the probabity of positive class

--- src/predict.py
+++ src/predict.py
@@ -86,7 +86,7 @@ def run_svm_18dims(datadir,gmtf,outdir,svmfile):
     #### classify ######
     comb_cls = svc.predict(rank_arr).astype(int)
     comb_prb = svc.predict_proba(rank_arr)   # 2 class, so 2-d array : probability to predict as 0 and 1
-    comb_prb = np.max(comb_prb,axis=1)   #  the probability to predict correctly
+    comb_prb = comb_prb[:,1]   #  the probability to predict correctly
     comb_dis = svc.decision_function(rank_arr)
     comb_score = - np.log( 1.0 - comb_prb  )
 
